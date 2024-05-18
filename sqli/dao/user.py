a3c8462a29891581b80cd1f9e6b02bcf522d39c0
ender0615 <87756536+ender0615@users.noreply.github.com>
2024-05-18 21:07:31 +0300

    Update user.py

--- sqli/dao/user.py
+++ sqli/dao/user.py
@@ -1,8 +1,9 @@
-from hashlib import md5
+from hashlib import scrypt
+from os import urandom
+from base64 import b64encode, b64decode
 from typing import NamedTuple, Optional
 
 from aiopg import Connection
-
 
 class User(NamedTuple):
     id: int
@@ -38,4 +39,13 @@ class User(NamedTuple):
             return User.from_raw(await cur.fetchone())
 
     def check_password(self, password: str):
-        return self.pwd_hash == md5(password.encode('utf-8')).hexdigest()
+        salt, hashed_password = self.pwd_hash.split('$')
+        salt = b64decode(salt)
+        hashed_input_password = scrypt(password.encode('utf-8'), salt=salt, n=16384, r=8, p=1, maxmem=0, dklen=64)
+        return hashed_input_password == b64decode(hashed_password)
+
+    @staticmethod
+    def hash_password(password: str) -> str:
+        salt = urandom(16)
+        hashed_password = scrypt(password.encode('utf-8'), salt=salt, n=16384, r=8, p=1, maxmem=0, dklen=64)
+        return f"{b64encode(salt).decode('utf-8')}${b64encode(hashed_password).decode('utf-8')}"
